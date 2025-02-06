0ffa24d66cfdb109af906d68a181efd225720176
Ryno Fourie <ryno@teamgeek.io>
2025-02-06 17:00:33 +0200

    feat: modal fix to open and close

--- resources/js/Pages/Shared/CreateModal.vue
+++ resources/js/Pages/Shared/CreateModal.vue
@@ -1,6 +1,10 @@
 <script setup>
 import { reactive } from 'vue'
+import { ref } from 'vue'
 import { router } from '@inertiajs/vue3'
+import Form from './Form.vue';
+
+defineProps(['role']);
 
 const form = reactive({
     first_name: null,
@@ -11,34 +15,43 @@ const form = reactive({
     phone_number: null,
 });
 
+// const showModal = ({
+//     show: false
+// });
+const showModal = ref(false);
+
+const handleModal = (bool) => {
+    return showModal.value = bool;
+}
 function submit() {
     router.post('/users', form)
 }
 </script>
 <template>
 
-    <div class="flex justify-end m-5 mr-20">
-        <button id="defaultModalButton" data-modal-target="defaultModal" data-modal-toggle="defaultModal"
-            class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-8 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            type="button">
+    <div class="flex justify-end m-5 mr-20" v-if="role === 'lead'">
+        <button
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-8 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            type="button" @click="handleModal(true)">
             Create
         </button>
     </div>
 
-    <div id="defaultModal" tabindex="-1" aria-hidden="true"
-        class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
+    <div
+        class="flex overflow-y-auto overflow-x-hidden fixed top-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full m-auto bg-black/70 "
+        :class="{'hidden': !showModal}">
         <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
 
-            <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
+            <div class="relative p-4 bg-white rounded-lg sm:p-5 shadow-xl">
 
                 <div
                     class="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600">
-                    <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                    <h3 class="text-lg font-semibold text-gray-900">
                         Add Product
                     </h3>
                     <button type="button"
                         class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                        data-modal-toggle="defaultModal">
+                        @click="handleModal(false)">
                         <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
                             xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd"
@@ -48,66 +61,8 @@ function submit() {
                         <span class="sr-only">Close modal</span>
                     </button>
                 </div>
-
-                <form @submit.prevent="submit">
-                    <div class="grid gap-4 mb-4 sm:grid-cols-2">
-                        <div>
-                            <label for="first_name"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First Name</label>
-                            <input type="text" name="firs_name" id="first_name" v-model="form.first_name"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                placeholder="Type product name" required="">
-                        </div>
-                        <div>
-                            <label for="last_name"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last Name</label>
-                            <input type="text" name="last_name" id="last_name" v-model="form.last_name"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                placeholder="Product brand" required="">
-                        </div>
-                        <div>
-                            <label for="id_number"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Id Number</label>
-                            <input type="number" name="id_number" id="id_number"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                placeholder="$2999" required="">
-                        </div>
-                        <div>
-                            <label for="role"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Role</label>
-                            <select id="role"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                                <option selected="lead">Lead</option>
-                                <option value="contact">Contact</option>
-                            </select>
-                        </div>
-                        <div>
-                            <label for="email"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                            <input type="number" name="email" id="email" v-mode="form.email"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                placeholder="$2999" required="">
-                        </div>
-                        <div>
-                            <label for="phone_number"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone Number</label>
-                            <input type="number" name="phone_number" id="phone_number" v-mode="form.phone_number"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                placeholder="$2999" required="">
-                        </div>
-                    </div>
-                    <button type="submit"
-                        class="text-white inline-flex items-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                        <svg class="mr-1 -ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                                d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                                clip-rule="evenodd"></path>
-                        </svg>
-                        Add new product
-                    </button>
-                </form>
+                <Form role="{role}" />
             </div>
         </div>
-    </div>
+    </div> 
 </template>
