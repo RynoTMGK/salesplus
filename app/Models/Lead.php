e0af601a00dcff5fe1be2c2d54b3cd9315b8be28
Ryno Fourie <ryno@teamgeek.io>
2025-02-14 15:05:23 +0200

    create lead model

--- app/Models/Lead.php
+++ app/Models/Lead.php
@@ -3,8 +3,37 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Lead extends Model
 {
-    //
+    protected $table = 'leads';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'id_number',
+        'email',
+        'phone_number',
+        'role',
+        'street_address',
+        'city',
+        'postal_code',
+        'province',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($lead) {
+            if (empty($lead->id)) {
+                $lead->id = Str::uuid();
+            }
+        });
+    }
 }
