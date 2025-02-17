d1bac69a98340f222e6f0c42ed4a0b5005919bf0
Ryno Fourie <ryno@teamgeek.io>
2025-02-17 09:12:31 +0200

    feat: condition to check roles to view button.

--- resources/js/Pages/Shared/CreateModal.vue
+++ resources/js/Pages/Shared/CreateModal.vue
@@ -15,7 +15,7 @@ const handleModal = (bool) => {
 </script>
 <template>
 
-    <div class="flex justify-end m-5 mr-20" v-if="role === 'lead'">
+    <div class="flex justify-end m-5 mr-20" v-if="role === 'lead' || role === 'contact'">
         <button
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-8 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             type="button" @click="handleModal(true)">
