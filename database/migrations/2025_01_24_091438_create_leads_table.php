11a440706b5e7311c491c27bafbc379dc6a6d554
Ryno Fourie <ryno@teamgeek.io>
2025-03-14 15:50:09 +0200

    feat: add new columns to table

--- database/migrations/2025_01_24_091438_create_leads_table.php
+++ database/migrations/2025_01_24_091438_create_leads_table.php
@@ -12,7 +12,19 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('leads', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('id_number', length:13);
+            $table->string('email')->unique();
+            $table->string('phone_number');
+            $table->string('street_address');
+            $table->string('city');
+            $table->string('postal_code');
+            $table->string('province');
+            $table->string('role', length:15)->default("lead");
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->string('status')->default("new");
             $table->timestamps();
         });
     }
