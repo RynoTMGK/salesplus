55d66878923b6cfa8208ea6115762110daf30195
Ryno Fourie <ryno@teamgeek.io>
2025-03-14 15:53:40 +0200

    feat: add a default role if sign up

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role', length:15)->default();
+            $table->string('role', length:15)->default("admin");
             $table->rememberToken();
             $table->timestamps();
         });
