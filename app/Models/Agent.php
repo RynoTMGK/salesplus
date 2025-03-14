25ecd70e76704e294ad2bb4e8756a9a636f801fc
Ryno Fourie <ryno@teamgeek.io>
2025-03-14 15:52:31 +0200

    feat:add model for agent

--- app/Models/Agent.php
+++ app/Models/Agent.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class Agent extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+
+    protected $table = 'users';
+
+    public function user() 
+    {
+        return $this->belongsTo(User::class);
+    }
 }
